f73ef1685cf1fce03ad533f45446cc6f4fdfbf87
JESSE <jessejzee@gmail.com>
2022-08-16 22:09:56 +0300

    first commit initial working with if, to replace switch case

--- week two/main.js
+++ week two/main.js
@@ -1,42 +1,50 @@
-var age = prompt("Enter your age");
+let userage = prompt("Enter your age");
 var msg="";
-switch (age){
-    case (age<18):
-    console.log("two young");
-    msg="a";
-    break;
-    case (age<27):
-    console.log("right age for military");
-    msg="b";
-    break;
-    case (age<41):
-        console.log("you are in reserve");
-        msg="c";
-        break;
-        case (age<55):
-            console.log("You are in backup reserved");
-            msg="d";
-            break;
-            default:
-                console.log("Too aged");
-                msg="e";
-             //   document.getElementById("agestatu").innerHTML=msg
+var age=parseInt(userage);
+console.log(age);
+{
+// switch (true){
+    
+//     case (age < 18):
+//     console.log("two young");
+//     msg="a";
+//     break;
+//     case (age < 27):
+//     console.log("right age for military");
+//     msg="b";
+//     break;
+//     case (age < 41):
+//         console.log("you are in reserve");
+//         msg="c";
+//         break;
+//     case (age < 55):
+//             console.log("You are in backup reserved");
+//             msg="d";
+//             break;
+//     default:
+//                 console.log("Too aged");
+//                 msg="e";
+//              //   document.getElementById("agestatu").innerHTML=msg
+// }
+
+if (age<18)
+{        msg="two young";
 }
-
-
-// if (age<18)
-// {
-//     console.log("two young");
-//     }
-//     if (age<27)
-//     {
-//         console.log("right age for military");
-//         }
-//         if (age<41)
-// {
-//     console.log("you are in reserve");
-//     }
-//     if (age<18)
-// {
-//     console.log("two young");
-//     }
+    else if (age<27)
+    {               msg="right age for military";
+    }
+    else if (age<41)
+    {
+               msg="you are in reserve";
+    } 
+    else if (age<55)
+    {
+        msg="You are in backup reserved";
+    }        
+    else
+    {
+        msg="Too aged";
+    }
+    console.log(msg);
+document.getElementById("agestatu").innerHTML=String(msg);
+}
