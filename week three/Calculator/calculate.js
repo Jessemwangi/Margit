50e3f422e2b4de8a80e3f115f401159b860941a5
JESSE <jessejzee@gmail.com>
2022-08-24 16:27:15 +0300

    done

--- week three/Calculator/calculate.js
+++ week three/Calculator/calculate.js
@@ -6,9 +6,20 @@ CalculateGas=()=>
 
     let money=+document.querySelector('#money').value;
     let ltrs=money/price;
- 
+ if (price=='' || price==null)
+ {
+    alert("HI, Seems that we are missing the price!, input price..");
+    document.getElementById("price").focus();
+    return false;
+ }
+ if (money=='' || money==null)
+ {
+    alert("HI, Seems that we are missing Money!, input money..");
+    document.getElementById("money").focus();
+    return false;
+ }
     if (ltrs>=10){
-        reader.textContent="Welcome: with the cash at hands, you can buy "+ ltrs+"L, good, you can escape now";
+        reader.textContent="Welcome: with the cash at hands, you can buy "+ ltrs+"L, its good, you can escape now";
         reader.style.color="#8ebf42"
     }
     else{
@@ -16,3 +27,4 @@ CalculateGas=()=>
     reader.style.color="#e75d07"
     }
 }
+
