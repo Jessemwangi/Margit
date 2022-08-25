e668a8919f1f50464173de200196a2441e538963
JESSE <jessejzee@gmail.com>
2022-08-25 21:51:23 +0300

    one line event listener to all checkboxes

--- week three/Calculator/PizzaOrder/pizza.js
+++ week three/Calculator/PizzaOrder/pizza.js
@@ -1,30 +1,30 @@
 let toppingcost=0, totalcost=0,deliverycost=0;toppingcount=0;
 let topplist="";
-function checkpep(){
-if (document.querySelector("#Pepperoni:checked"))
-{
-console.log(document.querySelector("#Pepperoni").value);
-topplist+=document.querySelector("#Pepperoni").value;
-toppingcount++
-}else{
-console.log("not checked");
-toppingcount--;
-}
-console.log(toppingcount);
-checkboxes();
-}
-let calculatecost= (topc,delic,topcount)=>
-{
-totalcost=topc*topcount+delic;
-};
+//add a function to all checkboxxes
+const elementsArray = document.querySelectorAll('input[type="checkbox"]');
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("change", function() {
+              checkboxes();
+    });
+});
 function checkboxes(){
+    topplist="";
     var inputElems = document.getElementsByTagName("input"),
     count = 0;
     for (var i=0; i<inputElems.length; i++) {
-    if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
-        count++;
         
+    if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
+        count++;    
+        topplist+=inputElems[i].value +", ";  
+        //console.log(topplist);  
     }
 }
-console.log(count);toppingcount=count;console.log(toppingcount);
+console.log(count);toppingcount=count;console.log(toppingcount);console.log(topplist); 
 }
+//calculate cost
+let calculatecost= (topc,delic,topcount)=>
+{
+    if (toppingcount>3){totalcost=topc*topcount+delic;}
+else{ }
+
+};
