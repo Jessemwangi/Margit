eeb2d118056af2ffb077accb035d644c22939bb8
Jessemwangi <jessejzee.com>
2022-08-26 21:58:16 +0300

    Started styling for Pizza asingnment, calculation done

--- week three/Calculator/PizzaOrder/pizza.js
+++ week three/Calculator/PizzaOrder/pizza.js
@@ -1,9 +1,7 @@
-let toppingcost=0, totalcost=0,deliverycost=0;toppingcount=0;
+let toppingcost=0, totalcost=0,deliverycost=0;pcikupmethod="", toppingcount=0;
 let topplist="", choicesizecost=0,choicesname="" , choiceprizecost=0;
-//add a function to all checkboxxes
 const elementsArray = document.querySelectorAll('input[type="checkbox"]');
 const radioelement=document.querySelectorAll('input[type="radio"]');
-document.querySelector('#deliver').addEventListener('change',delivery);
 elementsArray.forEach(function(elem) {
     elem.addEventListener("change", function() {
               checkboxes();
@@ -17,16 +15,17 @@ radioelement.forEach(function(elem){
         else if(choicesizecost==4){ choicesname="For 4, the price is 10,50€";choiceprizecost=10.50; }
         else if(choicesizecost==6){ choicesname="For 6, the price is 12,50€";choiceprizecost=12.50; }
         else if(choicesizecost==8){ choicesname="For 8, the price is 15,50€";choiceprizecost=15.50; }
-        
         console.log(choicesizecost,choicesname);
-        calculatecost(choicesname,choiceprizecost,topplist,toppingcount);
+        calculatecost(choicesname,choiceprizecost,topplist,toppingcount,deliverycost,pcikupmethod);
     });
 });
 let delivery=(e)=>{
-    console.log(e.se);
+    e = document.getElementById("deliver");
+    if(e.value==5){ deliverycost=5}
+    else{deliverycost=0}
+pcikupmethod = e.options[e.selectedIndex].text;  
+calculatecost(choicesname,choiceprizecost,topplist,toppingcount,deliverycost,pcikupmethod);
 }
-
-
 function checkboxes(){
     topplist="";
     var inputElems = document.getElementsByTagName("input"),
@@ -36,20 +35,18 @@ function checkboxes(){
     if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
         count++;    
         topplist+=inputElems[i].value +", ";  
-        //console.log(topplist);  
+        toppingcount=count; 
     }
 }
-//console.log(count);toppingcount=count;console.log(toppingcount);console.log(topplist); 
-calculatecost(choicesname,choiceprizecost,topplist,toppingcount);}
-
-//calculate cost
-let calculatecost= (choicesname,choiceprizecost,topplist,toppingcount)=>
+calculatecost(choicesname,choiceprizecost,topplist,toppingcount,deliverycost,pcikupmethod);}
+let calculatecost= (choicesname,choiceprizecost,topplist,toppingcount,deliverycost,pcikupmethod)=>
 {
     if (toppingcount>4) {
         toppingcost=((toppingcount-4)*0.5);}
     else{ toppingcost=0;}
-    totalcost=(toppingcost)+choiceprizecost;
+    totalcost=(toppingcost)+choiceprizecost+deliverycost;
 document.querySelector('#Ordersize').textContent=choicesname;
 document.querySelector('#toppings').textContent=topplist;
 document.querySelector('#Ptotalcost').textContent=totalcost;
+document.querySelector ('#pickup').textContent=pcikupmethod;
 };
