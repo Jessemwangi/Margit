15d7fd6211c5c4add20b01a51716b1daa0cb46ef
JESSE <jessejzee@gmail.com>
2022-09-09 22:31:09 +0300

    validation on car database

--- Tasks/cardatabse/main.js
+++ Tasks/cardatabse/main.js
@@ -1,5 +1,4 @@
 let forms=document.querySelector('#form');
-let savecar=(e)=>{
 let table1=document.querySelector('#database');
  let licence=document.querySelector('#licenceNumber').value;
  let maker=document.querySelector('#Carmaker').value;
@@ -7,6 +6,48 @@ let table1=document.querySelector('#database');
  let owner=document.querySelector('#carowner').value;
  let carprice=document.querySelector('#carprice').value;
  let carcolor=document.querySelector('#carcolor').value;
+ 
+let savecar=(e)=>{
+        let licence=document.querySelector('#licenceNumber').value;
+        let maker=document.querySelector('#Carmaker').value;
+        let model=document.querySelector('#carmodel').value;
+        let owner=document.querySelector('#carowner').value;
+        let carprice=document.querySelector('#carprice').value;
+        let carcolor=document.querySelector('#carcolor').value;
+        if (!licence)
+ {
+        alert('Value missing, Enter licence');
+        document.querySelector('#licenceNumber').focus();
+        return false;
+        }
+ else if (!maker)
+ {
+        alert('Value missing, , Enter maker');
+        document.querySelector('#Carmaker').focus();
+       return false;
+ }
+ else if(!model) {
+        alert('Value missing, , Enter maker');
+        document.querySelector('#carmodel').focus();
+       return false;
+ }
+ else if (!owner){
+        alert('Value missing,  Enter owner');
+        document.querySelector('#carowner').focus();
+       return false;
+ }
+ else if(!carprice){
+        alert('Value missing, Enter carprice');
+        document.querySelector('#carprice').focus();
+       return false;
+ }
+ else if(!carcolor){
+        alert('entity missing, Enter car color');
+        document.querySelector('#carcolor').focus();
+       return false;
+ }
+ else{
+
 
     e.preventDefault();
     console.log(licence,maker,model,owner);
@@ -23,5 +64,6 @@ cell1.innerHTML = licence;
         cell4.innerHTML = owner;
         cell5.innerHTML = carprice;
         cell6.innerHTML = carcolor;
+ }
 }
 forms.addEventListener('submit',savecar)
