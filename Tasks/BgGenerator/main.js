3598fb3b101f3498dd3b6e77529914dfea601352
Jessemwangi <jessejzee.com>
2022-09-13 13:27:18 +0300

    added gardient to body background

--- Tasks/BgGenerator/main.js
+++ Tasks/BgGenerator/main.js
@@ -2,47 +2,77 @@ const startcolor=document.querySelector('#color1');
 const endcolor=document.querySelector('#color2');
 const allradios=document.querySelectorAll('input[type="radio"]');
 const displaygrad=document.getElementById('outputdiv')
+
 let fillzone=displaygrad.getContext('2d');
-let colorgrade ;
+
+let colorgrade ;let direction;
+
 allradios.forEach(function (elem){
-elem.addEventListener('change' ,function(e){
-let selectedradio=e.target.value;
+elem.addEventListener('change' ,function (e){
+    selectedcolor(e.target.value);
+});
+});
+
+const selectedcolor = (selectedradio)=>{
+
 switch (selectedradio){
-
     case 'SE'://direction south east 
-    loadgradient(0,0,300,150);
-        break;
-            case 'DS'://direction south
-            loadgradient(150,0,150,150);
-        break;
-            case 'Sw'://direction south west
-            loadgradient(300,0,150,150);
+        loadgradient(0,0,300,150);
+        direction='to bottom right';
+    break;
 
-                break;
+    case 'DS'://direction south
+        loadgradient(150,0,150,150);
+        direction='to bottom';
+    break;
+
+    case 'Sw'://direction south west
+        loadgradient(300,0,150,150);
+        direction='to bottom left';
+    break;
     case 'Dright'://direction right
-    loadgradient(0,75,300,75);
-break;
-case 'Dleft'://direction left
-    loadgradient(300,75,0,75);
+        loadgradient(0,75,300,75);
+        direction='to right';
+    break;
+    case 'Dleft'://direction left
+        loadgradient(300,75,0,75);
+        direction='to left';
     break;
     case 'NE'://direction north east
-    loadgradient(0,150,300,0);
+        loadgradient(0,150,300,0);
+        direction='to top right';
     break;
     case 'DW'://direction north
-    loadgradient(150,75,150,0);
+        loadgradient(150,75,150,0);
+        direction='to top';
     break;
     case 'NW'://direction north west
-    console.log(selectedradio);
-    loadgradient(300,150,0,0);
+        loadgradient(300,150,0,0);
+        direction='to top left';
     break;
             default:
 }
-});
-});
-let loadgradient=(x0,yo,x1,y1)=>{
+
+let gradOutput = 'linear-gradient('+ direction+', '+ startcolor.value+', ' +endcolor.value+')';
+document.body.style.backgroundImage = gradOutput;
+
+document.querySelector('p').textContent = gradOutput;
+
+}
+
+const loadgradient=(x0,yo,x1,y1)=>{
     colorgrade = fillzone.createLinearGradient(x0,yo,x1,y1);
     colorgrade.addColorStop(0,startcolor.value);
     colorgrade.addColorStop(1,endcolor.value);
     fillzone.fillStyle = colorgrade;
     fillzone.fillRect(0,0,300,150);
+}
+
+const backgroundcolor = (Colordirection) => {
+    let gradOutput= 'linear-gradient('+ Colordirection+', '+ startcolor.value+', ' +endcolor.value+');';
+    document.body.style.backgroundImage=gradOutput;
+
+    document.querySelector('p').textContent=gradOutput;
+
+
 }
